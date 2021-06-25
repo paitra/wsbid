37fa25a20060a3639ee79d33adfa02d8e06004cd
paitra <paitra@users.noreply.github.com>
2021-06-24 21:36:14 -0300

    marge

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2549f3334a68606f61762581f29018d99599b64d',
+    'name' => 'codeigniter/framework',
+  ),
+  'versions' => 
+  array (
+    'chriskacerguis/codeigniter-restserver' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ced70d12c7e8d207eaded3f208a201330950b57',
+    ),
+    'codeigniter/framework' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2549f3334a68606f61762581f29018d99599b64d',
+    ),
+  ),
+);
